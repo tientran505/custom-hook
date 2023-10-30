e5b68662c874f06d09af7f071c8bdac513f4189c
Thu Trang Le <lthutrang17@gmail.com>
2023-10-30 17:38:26 +0700

    thu trang submition

--- src/hooks/useQueue.js
+++ src/hooks/useQueue.js
@@ -1,12 +1,36 @@
+import { useState } from 'react'
 const useQueue = (initialData = []) => {
-  
+  const [queue, setQueue] = useState(initialData);
+  const [top, setTop] = useState(queue[queue.length - 1]);
+  const [size, setSize] = useState(queue.length);
+
+  const push = (item) => {
+    setQueue([...queue, item]);
+    setSize(queue.length + 1);
+    setTop(item);
+  };
+  const pop = () => {
+    const fistItem = queue[0];
+    const newQueue = queue.slice(1);
+    setQueue(newQueue);
+    setSize(newQueue.length);
+    setTop(newQueue[newQueue.length - 1]);
+    return fistItem;
+
+  }
+  const clear = () => {
+    setQueue([]);
+    setSize(0);
+    setTop(undefined);
+  };
+
 
   return {
     push,
     pop,
     clear,
-    top: ,
-    size: ,
+    top,
+    size,
     queue,
   };
 };
