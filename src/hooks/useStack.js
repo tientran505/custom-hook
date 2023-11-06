0d892f0c8aef60401cfd61cda70db88cf1c6eafa
codemh178 <maikhanhhuyen91@gmail.com>
2023-11-07 00:31:19 +0700

    first commit

--- src/hooks/useStack.js
+++ src/hooks/useStack.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import { useState, useCallback } from 'react'
 
 const useStack = (initalData = []) => {
+  const [data, setData] = useState(initalData);
+
+  const push = useCallback((item) => {
+    setData((prevData) => [...prevData, item]);
+  }, []);
+
+  const top = data.length > 0 ? data[data.length - 1] : null
+
+  const pop = () => {
+    if (data.length > 0) {
+      setData(data.slice(0, -1))
+    }
+  }
   
+  const clear = () => {
+    setData([])
+  }
+
+  const size = data.length
+
+  const stack = data
+
   return {
     push,
     pop,
